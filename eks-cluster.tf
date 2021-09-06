b658417a32fdde64c39a6608c9146defc4b8dde6
Dincho Todorov <dincho@users.noreply.github.com>
2021-09-06 17:38:05 +0300

    Use AWS role during k8s authentication

--- eks-cluster.tf
+++ eks-cluster.tf
@@ -85,6 +85,20 @@ data "aws_eks_cluster_auth" "cluster" {
 }
 
 provider "kubernetes" {
+  exec {
+    api_version = "client.authentication.k8s.io/v1alpha1"
+    command     = "aws"
+    args = [
+      "--region",
+      "eu-central-1",
+      "eks",
+      "get-token",
+      "--cluster-name",
+      module.eks.cluster_id,
+      "--role",
+      aws_iam_role.cluster_admin.arn
+    ]
+  }
   host                   = data.aws_eks_cluster.cluster.endpoint
   cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
   token                  = data.aws_eks_cluster_auth.cluster.token
