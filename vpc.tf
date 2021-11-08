8383c1c708cab7b740bb1929ff1710f4fa64fab0
Petar Peshev <petar.peshev@leanplum.com>
2021-11-08 13:41:17 +0200

    make vpc variabels workspace related

--- vpc.tf
+++ vpc.tf
@@ -3,10 +3,10 @@ module "vpc" {
   version = "3.2.0"
 
   name                 = "${local.env}-vpc"
-  cidr                 = "10.0.0.0/16"
+  cidr                 = local.config.cidr
   azs                  = data.aws_availability_zones.available.names
-  private_subnets      = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-  public_subnets       = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
+  private_subnets      = local.config.private_subnets
+  public_subnets       = local.config.public_subnets
   enable_nat_gateway   = true
   single_nat_gateway   = true
   enable_dns_hostnames = true
