96cc6c15692de7017c9d7899739b43083dd5490f
Petar Peshev <petar.peshev@leanplum.com>
2021-11-18 22:46:56 +0200

    change vpc outputs

--- outputs.tf
+++ outputs.tf
@@ -10,10 +10,10 @@ output "vpc_id" {
   value = module.vpc.vpc_id
 }
 
-output "vpc_private_subnets" {
+output "private_subnets" {
   value = module.vpc.private_subnets
 }
 
-output "vpc_public_subnets" {
+output "public_subnets" {
   value = module.vpc.public_subnets
 }
