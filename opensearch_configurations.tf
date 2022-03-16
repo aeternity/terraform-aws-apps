14872dd92a838d9293fdc8ebd5b82962853ca97d
Petar Peshev <petar.peshev@leanplum.com>
2022-03-16 21:21:16 +0200

    add opensearch configurations script arguments

--- opensearch_configurations.tf
+++ opensearch_configurations.tf
@@ -1,5 +1,5 @@
 resource "null_resource" "opensearch_configurations" {  
   provisioner "local-exec" {
-    command = "./scripts/opensearch_configurations.sh ${local.opensearch_master_user_password} ${var.opensearch_master_user} ${local.cluster_name} ${module.eks.cluster_iam_role_arn} ${module.eks.worker_iam_role_arn}"
+    command = "./scripts/opensearch_configurations.sh ${local.opensearch_master_user_password} ${var.opensearch_master_user} ${local.cluster_name} ${module.eks.cluster_iam_role_arn} ${module.eks.worker_iam_role_arn} fluent-bit fluentbit"
   }
 }
