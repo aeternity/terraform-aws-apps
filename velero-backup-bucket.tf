d1abd432ea75f78610f2feb87d963f122cc64340
Petar Peshev <petar.peshev@leanplum.com>
2022-04-07 09:46:54 +0300

    change velero bucket name

--- velero-backup-bucket.tf
+++ velero-backup-bucket.tf
@@ -1,9 +1,9 @@
 module "s3_bucket_velero_backup" {
   source = "terraform-aws-modules/s3-bucket/aws"
 
-  bucket = "velero-backup-${local.env_human}"
+  bucket = "aeternity-velero-backup-${local.env_human}"
   acl    = "private"
-
+  
   versioning = {
     enabled = false
   }
