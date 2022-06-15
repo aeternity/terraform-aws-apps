ad03b2c4f6651f780fb127966d240d376eb6399b
Petar Peshev <petar.peshev@leanplum.com>
2022-06-15 10:29:36 +0300

    fix opensearch scripts path

--- opensearch_configurations.tf
+++ opensearch_configurations.tf
@@ -1,11 +1,11 @@
 resource "null_resource" "opensearch_configurations_fluentbit" {  
   provisioner "local-exec" {
-    command = "./scripts/opensearch_configurations.sh ${local.opensearch_master_user_password} ${var.opensearch_master_user} ${local.cluster_name} ${module.eks.cluster_iam_role_arn} ${module.eks.worker_iam_role_arn} fluent-bit fluentbit"
+    command = "./modules/terraform-aws-opensearch/scripts/opensearch_configurations.sh ${local.opensearch_master_user_password} ${var.opensearch_master_user} ${local.cluster_name} ${module.eks.cluster_iam_role_arn} ${module.eks.worker_iam_role_arn} fluent-bit fluentbit"
   }
 }
 
 resource "null_resource" "opensearch_configurations_kube_events" {  
   provisioner "local-exec" {
-    command = "./scripts/opensearch_configurations.sh ${local.opensearch_master_user_password} ${var.opensearch_master_user} ${local.cluster_name} ${module.eks.cluster_iam_role_arn} ${module.eks.worker_iam_role_arn} k8s-events k8sevents"
+    command = "./modules/terraform-aws-opensearch/scripts/opensearch_configurations.sh ${local.opensearch_master_user_password} ${var.opensearch_master_user} ${local.cluster_name} ${module.eks.cluster_iam_role_arn} ${module.eks.worker_iam_role_arn} k8s-events k8sevents"
   }
 }
