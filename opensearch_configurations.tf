ec871ed5917a2f7c5a2dd8effcd87169d7163c4e
Petar Peshev <petar.peshev@leanplum.com>
2022-09-22 23:43:45 +0300

    fix special chars in local exec bash arguments

--- opensearch_configurations.tf
+++ opensearch_configurations.tf
@@ -1,17 +1,17 @@
 resource "null_resource" "opensearch_configurations_fluentbit" {  
   provisioner "local-exec" {
-    command = "./modules/terraform-aws-opensearch/scripts/opensearch_configurations.sh ${local.opensearch_master_user_password} ${var.opensearch_master_user} ${local.cluster_name} ${module.eks.cluster_iam_role_arn} ${module.eks.worker_iam_role_arn} fluent-bit fluentbit"
+    command = "./modules/terraform-aws-opensearch/scripts/opensearch_configurations.sh '${local.opensearch_master_user_password}' ${var.opensearch_master_user} ${local.cluster_name} ${module.eks.cluster_iam_role_arn} ${module.eks.worker_iam_role_arn} fluent-bit fluentbit"
   }
 }
 
 resource "null_resource" "opensearch_configurations_kube_events" {  
   provisioner "local-exec" {
-    command = "./modules/terraform-aws-opensearch/scripts/opensearch_configurations.sh ${local.opensearch_master_user_password} ${var.opensearch_master_user} ${local.cluster_name} ${module.eks.cluster_iam_role_arn} ${module.eks.worker_iam_role_arn} k8s-events k8sevents"
+    command = "./modules/terraform-aws-opensearch/scripts/opensearch_configurations.sh '${local.opensearch_master_user_password}' ${var.opensearch_master_user} ${local.cluster_name} ${module.eks.cluster_iam_role_arn} ${module.eks.worker_iam_role_arn} k8s-events k8sevents"
   }
 }
 
 resource "null_resource" "opensearch_configurations_fluentbitHetzner" {  
   provisioner "local-exec" {
-    command = "./modules/terraform-aws-opensearch/scripts/opensearch_configurations.sh ${local.opensearch_master_user_password} ${var.opensearch_master_user} ${local.cluster_name} ${module.eks.cluster_iam_role_arn} ${module.eks.worker_iam_role_arn} fluent-hetzner fluentbitHetzner"
+    command = "./modules/terraform-aws-opensearch/scripts/opensearch_configurations.sh '${local.opensearch_master_user_password}' ${var.opensearch_master_user} ${local.cluster_name} ${module.eks.cluster_iam_role_arn} ${module.eks.worker_iam_role_arn} fluent-hetzner fluentbitHetzner"
   }
 }
