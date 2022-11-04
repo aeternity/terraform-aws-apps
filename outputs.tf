ce8ce1916356813ae27126312d5cd52256b4dfc8
Dincho Todorov <dincho@users.noreply.github.com>
2022-11-04 12:16:50 +0200

    Remove ingress certificate

--- outputs.tf
+++ outputs.tf
@@ -2,10 +2,6 @@ output "irsa_role_arn" {
   value = module.aws-lb-controller-role.iam_role_arn
 }
 
-output "ingress_cert_arn" {
-  value = aws_acm_certificate.ingress.arn
-}
-
 output "vpc_id" {
   value = module.vpc.vpc_id
 }
